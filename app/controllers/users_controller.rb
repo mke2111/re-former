36560c4e70861639e102c296d74ff40e1b912eb0
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-13 17:59:47 -0300

    Add edit and update methods and edit html file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,25 @@ class UsersController < ApplicationController
 
 
     saved = @user.save
-    puts "Saved = #{saved}"
 
     if saved
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
