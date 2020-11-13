b03a06628c5f9aa6212c4ccb18d8d3bb86343930
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-13 14:24:41 -0300

    Add method to create a user without nest user in html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    
+    saved = @user.save
+    puts "Saved = #{saved}"
+    
+    if saved
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
